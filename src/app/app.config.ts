7cfa1e779f5e0e2da2aaf0f9bc27d890cbaf86b2
babypussct <oneloveonepeopleforever@gmail.com>
2026-01-12 12:25:01 +0700

    FIX

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZonelessChangeDetection(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation(), withComponentInputBinding())
   ]
 };
