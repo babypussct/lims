d01f92f1c33f9164dac2a357603c1fab289b29f8
babypussct <oneloveonepeopleforever@gmail.com>
2026-04-09 23:51:39 +0700

    feat: implement data archiver feature to export and clear old Firebase records and add PWA support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withHashLocation, withComponentInputBinding, withRouterConfig } from '@angular/router';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,9 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(),
       // Tối ưu hoá Navigation Stack nếu người dùng huỷ chuyển trang (cancel loading module)
       withRouterConfig({ canceledNavigationResolution: 'replace' })
-    )
+    ), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
