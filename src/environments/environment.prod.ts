c9ed8cd25241595951efebce54e4abed79eba20b
babypussct <oneloveonepeopleforever@gmail.com>
2026-05-11 23:19:44 +0700

    chore: update firebase vapidKey in environment configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
     messagingSenderId: "498845778988",
     appId: "1:498845778988:web:e20c971a3af3a1ca5bfd89",
     measurementId: "G-M02RLXX5GD",
-    vapidKey: "n__OMcy7HyAg3kn74xK2Xn2bnpDYuQXY2vUELH-04aM"
+    vapidKey: "BEJBj3PRrZPhGdRP6cYgi56Bh83VNpYyhwO72DEKQmCKZG72kuS1uJ3wQdoeVJOEnpdqyuQgcwG4dOHPAg_VNDM"
   },
   googleDrive: {
     apiKey: 'AIzaSyDYLmYAso4LXf392Y__EGj7EEu4F4G_giE',
